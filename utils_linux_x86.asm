6e0af0c118dc6c351c0b69e6f046d6ed960a801b
-TOSH- <tosh@t0x0sh.org>
2015-06-06 14:18:17 +0200

    Forget to add include file

---
+++ utils_linux_x86.asm
@@ -0,0 +1,17 @@
+%define SYS_EXIT 1
+%define SYS_FORK 2
+%define SYS_READ 3
+%define SYS_WRITE 4
+%define SYS_OPEN 5
+%define SYS_CLOSE 6
+%define SYS_EXECV 11
+%define SYS_CHDIR 12
+%define SYS_SETUID 23
+%define SYS_PTRACE 26
+%define SYS_DUP2 63
+%define SYS_SOCKETCALL 102
+
+
+%define SOCK_SOCKET 1
+%define SOCK_CONNECT 3
+
